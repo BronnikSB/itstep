09531cb1cf7c02b6413b8cc57009ed4c906c60dc
Sergey Bronnikov <bronks.work@gmail.com>
2021-09-28 21:14:58 +0300

    turple

---
+++ cherepaha.py
@@ -0,0 +1,90 @@
+# from turtle import *
+# color('red','yellow')
+# begin_fill()
+# while True:
+#     forward(100)
+#     left(170)
+#     if abs(pos())<1:
+#         break
+# end_fill()
+# mainloop()
+#---------------------------------------------
+# from turtle import *
+# speed(100)
+# for i in range (1, 300, 2):
+#     forward(i)
+#     left(i)
+#--------------------------------------------
+# from turtle import *
+#
+# shape("turtle")
+# speed(0.6)
+# forward(50)
+# left(180)
+# right(53.13)
+# forward(30)
+# left(90)
+# forward(40)
+#--------------------------------------------
+# Движение черепахи
+# Двигаться и рисовать
+# forward() | fd()
+# backward() | bk() | back()
+# right() | rt()
+# left() | lt()
+# goto() | setpos() | setposition()
+# setx()
+# sety()
+# setheading() | seth()
+# home() Переместить черепаху в исходную точку
+# circle() круг
+# dot() нарисовать точку в аргументах передавать размер и цвет
+#
+# speed()
+# Рассказать о состоянии Черепахи
+# position() | pos()
+#
+# distance()
+# Управление Pen
+# Состояние рисования
+# pendown() | pd() | down() опустить карандаш
+# penup() | pu() | up() поднять карандаш
+# pensize() | width() размер карандаша
+#
+#
+# Управление цветом
+# color()
+# pencolor()
+# fillcolor()
+# Заполнение
+# filling()
+# begin_fill()
+# end_fill()
+# Больше контроля рисования
+# reset()
+# clear()
+# write()
+# Состояние черепахи
+# Видимость
+# showturtle() | st()
+# hideturtle() | ht()
+# isvisible()
+# Внешний вид
+# shape() форма
+#
+# shapesize() | turtlesize() размер формы
+#
+#
+# Методы TurtleScreen/Screen¶
+# Управление окном
+# bgcolor() цвет фона
+# bgpic() картинка фона
+# clear() | clearscreen()
+# reset() | resetscreen()
+# screensize() размер экрана
+# Управление анимацией
+# delay()
+# tracer()
+# update()
+#
+# mainloop() | done() задержка экрана
